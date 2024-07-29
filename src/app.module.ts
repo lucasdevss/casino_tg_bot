d1a527c5027a996198706042ceb7d114a5cd1ab0
FlammaSoft Team <lucasdev1981@gmail.com>
2024-07-29 12:36:25 +0200

    Add healthcheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BotModule } from './bot/bot.module';
 import { I18nTranslateModule } from './i18n/i18n.module';
+import { HealthModule } from './shared/health';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     BotModule,
     I18nTranslateModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
